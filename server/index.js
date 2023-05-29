c2b52b37be538ca3bf5b7c6175515409cf300a5a
Eeshaan Sethia <eeshaans05@gmail.com>
2023-05-30 02:54:53 +0530

    Added a backend

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT;
+app.use(cors());
+
+mongoose.set('strictQuery', false)
+mongoose.connect('mongodb://127.0.0.1:27017/geetanjaliPapad', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to the Geetanjali Papad',
+        status: 200
+    });
+});
+
+app.listen(port);
---
+++ server/models/papadDetails.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const papadDetails = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String
+    },
+    pricing: {
+        type: Number,
+    },
+    ingredients: {
+        type: Object
+    },
+    values: {
+        type: Number
+    },
+});
+
+module.exports = mongoose.model('papadDetails', papadDetails, papadDetails);
